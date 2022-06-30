d43bac49a77bd5eb4ee013bea4ac439583eb819f
ryanmuhl <ryanmuhl@hotmail.com>
2022-06-30 18:11:56 -0400

    push to repository

---
+++ client/src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+
+
+const Home = () => {
+
+  return (
+    <main>
+        <div class="text-primary">
+        Hello!!!!
+        </div>
+    </main>
+  );
+};
+
+export default Home;
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 
+
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
@@ -20,7 +21,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
