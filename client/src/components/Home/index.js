1f0925fbe34ba4fce3505e43742ebc7affbd5d3e
Your Name <felix.acevedo.tech@gmail.com>
2022-07-01 17:30:54 -0400

    fixing bg img and footer

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import coverImage from '../../assets/cover/cover-image.jpg';
+// import coverImage from '../../assets/cover/cover-image.jpg';
 // import avatar from '../../assets/avatar/avatar.jpg'
 import { capitalizeFirstLetter } from "../../utils/helpers";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -20,26 +20,27 @@ function Home() {
   const [currentPage] = useState(pages[0]);
 
   return (
-    <section className="home-example">
+    // <section className="home-example">
       
-        <div className="card border-0">
-          <img className="card-img-top vh-100" src={coverImage} alt="light bulb"></img>
-          <div className="card-img-overlay card-inverse home-content scroll-text">
-            <h1 className="home-title" style={{ fontSize: "40px" }}>{capitalizeFirstLetter(currentPage.name)}</h1>
-            {/* <img className="avatar-img" src={avatar} alt="avatar"></img> */}
-            <div>            
-              <p className="home-desc home-text">{currentPage.description}</p>
-            </div>
-          </div>
+    //     <div className="card border-0">
+    //       <img className="card-img-top vh-100" src={coverImage} alt="light bulb"></img>
+    //       <div className="card-img-overlay card-inverse home-content scroll-text">
+    //         <h1 className="home-title" style={{ fontSize: "40px" }}>{capitalizeFirstLetter(currentPage.name)}</h1>
+    //         {/* <img className="avatar-img" src={avatar} alt="avatar"></img> */}
+    //         <div>            
+    //           <p className="home-desc home-text">{currentPage.description}</p>
+    //         </div>
+    //       </div>
+    //     </div>
+    // </section>
+
+    <section className="hero">
+        <div className="hero-cta">
+            <h2>{capitalizeFirstLetter(currentPage.name)}</h2>
+            <p className="home-desc home-text">{currentPage.description}</p>
         </div>
     </section>
-  
-    // <Container>
-    //   <Row>
-    //     <Col></Col>
-    //     <Col></Col>
-    //   </Row>
-    // </Container>
+    
   );
 }
 
