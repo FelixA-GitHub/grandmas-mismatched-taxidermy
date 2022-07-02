7e07a63e6d469e2084d07281933692e95ea9eff9
Obtains <97128583+Obtains@users.noreply.github.com>
2022-07-02 15:33:55 -0400

    created functionality to mutations.js

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,23 @@
+import gql from 'graphql-tag';
+
+export const ADD_USER = gql`
+mutation addUser( $username: String!, $password: String!, $email: String! ) {
+    addUser( username: $username, password: $password, email: $email ) {
+        token 
+        user {
+            _id username
+        }
+    }
+}
+`;
+
+export const LOGIN_USER = gql`
+mutation loginUser( $email: String!, $password: String! ) {
+    login( email: $email, password: $password ) {
+        token
+        user {
+            _id
+        }
+    }
+}
+`;
