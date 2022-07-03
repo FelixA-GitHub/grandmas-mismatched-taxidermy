164a985e492b9e6752998171c4e251dac825ba09
Your Name <felix.acevedo.tech@gmail.com>
2022-07-02 20:05:21 -0400

    added client side auth

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 import { LOGIN_USER } from '../../utils/mutations';
-import Auth from '../../../../server/utils/auth';
+import Auth from '../../utils/auth';
 import { useMutation } from '@apollo/react-hooks';
 
 const Login = () => {
     const [userFormData, setUserFormData] = useState({ email: '', password: '' });
     const [validated] = useState(false);
     const [showAlert, setShowAlert] = useState(false);
-    const [loginUser] = useMutation(LOGIN_USER);
+    const [loginUser, { error }] = useMutation(LOGIN_USER);
 
     const [pages] = useState([
         {
