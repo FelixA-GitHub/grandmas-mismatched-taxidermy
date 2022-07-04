a80625b7b55e9aa4fd1db0c59fe5a98daec5e772
Your Name <felix.acevedo.tech@gmail.com>
2022-07-04 19:03:07 -0400

    added signup page

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import { setContext } from '@apollo/client/link/context';
 import Home from './components/Home';
 import Login from './pages/Login';
 import NoMatch from './pages/NoMatch';
+import Signup from './pages/Signup';
 import Header from './components/Header';
 import Nav from './components/Nav';
 import Page from './components/Page';
