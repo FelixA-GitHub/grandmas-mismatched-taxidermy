73c740816937f0c39307e0e42c4a4e27927b15f9
Obtains <97128583+Obtains@users.noreply.github.com>
2022-07-04 11:37:02 -0400

    finished controller for users

--- server/controllers/user-controllers.js
+++ server/controllers/user-controllers.js
@@ -1,1 +1,38 @@
 const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+module.exports = {
+    async getSingleUser({ user = null, params }, res) {
+        const findUser = await User.findOne({
+            $or: [{ _id: user ? user._id: params.id },
+            { username: params.username }]
+        });
+        if (!findUser) {
+            return res.status(400).json({ message: 'User not found!'});
+        }
+        res.json(findUser);
+    },
+    async createUser({ body }, res) {
+        const user = await User.create(body);
+        if (!user) {
+            return res.status(400).json({ message: 'Error...! Bugged...!' });
+        }
+        const token = signToken(user);
+        res.json({ token, user });
+    },
+    async login({ body }, res) {
+        const user = await User.findOne({
+            $or: [{ username: body.username },
+            { email: body.email }]
+        });
+        if (!user) {
+            return res.status(400).json({ message: 'User not Found!' });
+        }
+        const correctPassword = await user.isCorrectPassword(body.password);
+        if (!correctPassword) {
+            return res.status(400).json({ message: 'Wrong Password!' });
+        }
+        const token = signToken(user);
+        res.json({ token, user });
+    }
+};
