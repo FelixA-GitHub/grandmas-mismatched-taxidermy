afbccea566a2081f3019b37fb5845beea38ffc40
ryanmuhl <ryanmuhl@hotmail.com>
2022-07-05 09:25:44 -0400

    update saleItem and Listings to append cards

--- client/src/components/SaleItem/index.js
+++ client/src/components/SaleItem/index.js
@@ -1,31 +1,31 @@
 import React from "react";
-import { removeHyphens, capitalizeFirstLetter } from "../../utils/helpers";
+// import { removeHyphens, capitalizeFirstLetter } from "../../utils/helpers";
 import 'bootstrap/dist/css/bootstrap.css';
+// import {useParams} from "react-router-dom"
+import { useQuery } from '@apollo/client';
+import { QUERY_ALL_ITEMS } from '../../utils/queries';
+import Listing from "../Listing";
 
-function SaleItem() {
+const SaleItem = () => {
+    
+
+    const { loading, data } = useQuery(QUERY_ALL_ITEMS);
+
+    const items = data?.items || [];
+    console.log(items)
 
     return (
-        <div className="container-fluid">
-            <div className="row my-2 justify-content-center">
-                <div className="card p-1" style={{ width: "18rem" }}>
-                    <img className="card-img-top" src="https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/grandma_elderly_nanny_avatar-512.png"
-                        alt="name" />
-                    {/* <div className="card-img-overlay example"> */}
-                        <div className="card-body content">
-                            <p className="card-text">description</p>
-                            <p className="card-text">price</p>
-                            <div>
-                                <a href="/">
-                                <i class="fas fa-money-bill-wave-alt"></i>
-                                </a>
-                            </div>
-
-                        </div>
-                    {/* </div> */}
-
-                </div>
-            </div>
-        </div>
+        <main>
+  <div className="flex-row justify-space-between">
+    <div className="col-12 mb-3">
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <Listing items={items} title="Some Feed for Thought(s)..." />
+      )}
+    </div>
+  </div>
+</main>
 
     );
 };
