edd6b0006a4a4275cd663a0cfd2d354d14958208
Obtains <97128583+Obtains@users.noreply.github.com>
2022-07-05 20:00:25 -0400

    created comments

---
+++ client/src/components/Comments/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const CommentList = ({ comments, title }) => {
+  if (!comments.length) {
+    return <h3>No Comments Yet</h3>;
+  }
+
+  return (
+    <div>
+      <h3>{title}</h3>
+      {comments &&
+        comments.map(comment => (
+          <div key={comment._id} className="card mb-3">
+            <p className="card-header">
+              <Link
+                to={`/profile/${comment.username}`}
+                style={{ fontWeight: 700 }}
+                className="text-light"
+              >
+                {comment.username}
+              </Link>{' '}
+              comment on {comment.createdAt}
+            </p>
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default CommentList;
