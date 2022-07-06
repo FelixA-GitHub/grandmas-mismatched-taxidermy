72ce6b091a9b43b64fae567ceeef062eac16708e
Your Name <felix.acevedo.tech@gmail.com>
2022-07-06 13:09:34 -0400

    comment section is a bugger

--- client/src/pages/Comment.js
+++ client/src/pages/Comment.js
@@ -1,75 +1,31 @@
 import React from 'react';
-import { Navigate, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+// import SaleCOMMENT from '../components/SaleCOMMENT';
 
-import CommentForm from '../components/CommentForm';
-import CommentList from '../components/CommentList';
-
+// import Auth from '../utils/auth';
 import { useQuery } from '@apollo/client';
-import { QUERY_USER } from '../utils/queries';
-import Auth from '../utils/auth';
+import { QUERY_ALL_COMMENTS } from '../utils/queries';
+import CommentForm from '../components/CommentForm';
 
-const Profile = (props) => {
-  const { username: userParam } = useParams();
-  
-  const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_USER, {
-    variables: { username: userParam },
+
+const Comment = (props) => {
+  const { id: commentId } = useParams();
+
+  const { loading, data } = useQuery(QUERY_ALL_COMMENTS, {
+    variables: { id: commentId },
   });
 
-  const user = data?.me || data?.user || {};
-
-  // navigate to personal profile page if username is yours
-  if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
-    return <Navigate to="/profile:username" />;
-  }
+  const comment = data?.comment || {};
 
   if (loading) {
     return <div>Loading...</div>;
   }
 
-  if (!user?.username) {
-    return (
-      <h4>
-        You need to be logged in to see this. Use the navigation links above to
-        sign up or log in!
-      </h4>
-    );
-  }
-
-//   const handleClick = async () => {
-//     try {
-//       await addFriend({
-//         variables: { id: user._id },
-//       });
-//     } catch (e) {
-//       console.error(e);
-//     }
-//   };
-
   return (
     <div>
-      <div className="flex-row mb-3">
-        <h2 className="bg-dark text-secondary p-3 display-inline-block">
-          Viewing {userParam ? `${user.username}'s` : 'your'} profile.
-        </h2>
-
-        {/* {userParam && (
-          <button className="btn ml-auto" onClick={handleClick}>
-            Add Friend
-          </button>
-        )} */}
-      </div>
-
-      <div className="flex-row justify-space-between mb-3">
-        <div className="col-12 mb-3 col-lg-8">
-          <CommentList
-            thoughts={user.thoughts}
-            title={`${user.username}'s thoughts...`}
-          />
-        </div>
-      </div>
-      <div className="mb-3">{!userParam && <CommentForm />}</div>
+      <CommentForm commentId={comment._id} />
     </div>
   );
 };
 
-export default Profile;
+export default Comment;
--- client/src/pages/SingleComment.js
+++ client/src/pages/SingleComment.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
-// import ReactionList from '../components/ReactionList';
-// import ReactionForm from '../components/ReactionForm';
 
-import Auth from '../utils/auth';
+// import Auth from '../utils/auth';
 import { useQuery } from '@apollo/client';
 import { QUERY_COMMENT } from '../utils/queries';
 
